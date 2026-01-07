08294e300b61e0078be37be013f69422312fccf7
Giles Bowkett <giles.bowkett@openplay.co>
2026-01-07 12:44:02 -0800

    fixture needs to use obj_as_argument twice

--- test/fixtures/object_as_argument.rb
+++ test/fixtures/object_as_argument.rb
@@ -1,5 +1,5 @@
 def some_method(obj_as_argument)
-  if some_condition? && argument&.query_method?
+  if some_condition? && obj_as_argument&.query_method?
     obj_as_argument.field = value
   end
 end
